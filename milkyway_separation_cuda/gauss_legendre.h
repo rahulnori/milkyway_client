d387029e26ceebc0b6aa9b9590d749cbe7bfcb07
Travis Desell <travis.desell@gmail.com>
2010-05-07 15:35:18 -0400

    CUDA milkyway separation application files

---
+++ milkyway_separation_cuda/gauss_legendre.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2008, 2009 Travis Desell, Dave Przybylo, Nathan Cole,
+ * Boleslaw Szymanski, Heidi Newberg, Carlos Varela, Malik Magdon-Ismail
+ * and Rensselaer Polytechnic Institute.
+ *
+ * This file is part of Milkway@Home.
+ *
+ * Milkyway@Home is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Milkyway@Home is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Milkyway@Home.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+
+#ifndef MILKWAY_GAUSS_LEGENDRE_H
+#define MILKWAY_GAUSS_LEGENDRE_H
+
+void d_gauss_legendre(double x1, double x2, double *x, double *w, int n);
+void f_gauss_legendre(float x1, float x2, float *x, float *w, int n);
+
+#endif
