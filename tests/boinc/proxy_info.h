a0b2b104983d9694283b00a0a91ace46dd602bb4
Anthony <watera2@cs.rpi.edu>
2010-05-14 22:35:11 -0400

    added the boinc directory for building (contains the header files and libraries for windows)

---
+++ tests/boinc/proxy_info.h
@@ -0,0 +1,43 @@
+// This file is part of BOINC.
+// http://boinc.berkeley.edu
+// Copyright (C) 2008 University of California
+//
+// BOINC is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// BOINC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _PROXY_INFO_
+#define _PROXY_INFO_
+
+#include "miofile.h"
+
+struct PROXY_INFO {
+    bool use_http_proxy;
+    bool use_socks_proxy;
+	bool use_http_auth;
+    int socks_version;
+    char socks_server_name[256];
+    char http_server_name[256];
+    int socks_server_port;
+    int http_server_port;
+    char http_user_name[256];
+    char http_user_passwd[256];
+    char socks5_user_name[256];
+    char socks5_user_passwd[256];	
+	char noproxy_hosts[256];
+    int parse(MIOFILE&);
+    int write(MIOFILE&);
+    void clear();
+};
+
+#endif
+
